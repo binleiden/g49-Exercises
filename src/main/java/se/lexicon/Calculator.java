78a6765584f04ce5ac8140eb123f6dfe80d76e07
binleiden <daniel.petersson83@gmail.com>
2024-02-27 14:42:16 +0100

    Created calculator

---
+++ src/main/java/se/lexicon/Calculator.java
@@ -0,0 +1,18 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Lets do some math!");
+        System.out.println("Enter two numbers and ill do the math for you!");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 - num2);
+    }
+}
