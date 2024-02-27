b7d80165926065c1e2a4daa92e36a65a8deaad3c
binleiden <daniel.petersson83@gmail.com>
2024-02-27 14:17:24 +0100

    Created Average and Operators program

---
+++ src/main/java/se/lexicon/Average.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+public class Average {
+    public static void main(String[] args) {
+        int a=21, b=64, c=5;
+        int sum = (a+b+c) / 3;
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/se/lexicon/Operators.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 54;
+        int num2 = 38;
+        System.out.println(num1 + num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 - num2);
+    }
+}
