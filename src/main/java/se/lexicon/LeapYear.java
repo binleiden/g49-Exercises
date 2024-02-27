f0ba4355c552d683a746fc039a2db9e5f15f8a5b
binleiden <daniel.petersson83@gmail.com>
2024-02-27 14:08:11 +0100

    Created a leapyear program

---
+++ src/main/java/se/lexicon/LeapYear.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a year to se if its a leapyer");
+        int year = scanner.nextInt();
+        if (year % 400 == 0){
+            System.out.println("Its a leapyear!");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("Its a leapyear!");
+        }else  {
+            System.out.println("Its not a leapyear!");
+        }
+    }
+}
