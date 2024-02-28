419c0bd12a9eef3a7121851e926dbb86f72eb0f1
binleiden <daniel.petersson83@gmail.com>
2024-02-28 10:12:44 +0100

    Hours coding ide

--- src/main/java/se/lexicon/Hours.java
+++ src/main/java/se/lexicon/Hours.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class Hours {
     public static void main(String[] args) {
+        // User puts in seconds to be converted
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter seconds and ill tell you how many hours it is!");
         int seconds = scanner.nextInt();
 
+        //divide seconds with 3600 to get hours
         double actualQuotient = (double)seconds / 3600;
         System.out.println(actualQuotient);
+        int H = seconds / 3600
+        //divide remaining with 60 to get minutes
+        remainder = % 60
+        int M = H % 60
+        // remaining is seconds
+        int S = M / 60
+        // sout dislay 00:00:00
     }
 }
